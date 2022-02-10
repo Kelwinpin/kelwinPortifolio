3ebc76a395e2c3233357407362f0ebe2e48d23f8
Kelwinpin <kelwincomw@gmail.com>
2022-02-10 10:49:08 -0300

    Adicionado funções aos botões e removendo o botão DIO e cousera

---
+++ Portifólio_kelwin/index.js
@@ -0,0 +1,11 @@
+function entrarInsta(){
+    window.location.href = "https://www.instagram.com/kelwin.pin/";
+}
+
+function entrarGitHub(){
+    window.location.href = "https://www.instagram.com/kelwin.pin/";
+}
+
+function entrarLinkedin(){
+    window.location.href = "https://web.dio.me/certificates";
+}
